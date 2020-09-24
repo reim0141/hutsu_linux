a807e784050906bb8247597eba1b8cd5fa06a65a
reim0141 <reim0141x@gmail.com>
2020-09-24 17:00:10 +0300

    add mv

---
+++ mv.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "%s: wrong arguments\n", argv[0]);
+  }
+
+  if (rename(argv[1], argv[2]) < 0) {
+    perror(argv[1]);
+    exit(1);
+  }
+  exit(0);
+}
